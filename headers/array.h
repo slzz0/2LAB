048f988a053e57f4b82c67f99fefff4e7d5eae13
slzz <kcc265641@gmail.com>
2025-09-15 22:57:19 +0300

    bugs

--- headers/array.h
+++ headers/array.h
@@ -1,23 +1,53 @@
 #pragma once
-
 #include <iostream>
+
+#include "../headers/utilits.h"
+
 
 class Array {
    private:
-    int* data;
-    int size;
+    int* data = nullptr;
+    int size = 0;
 
     void allocateMemory(int size);
     void freeMemory();
 
    public:
-    Array();
-    Array(int size);
+    Array() = default;
+    explicit Array(int array_size);
     ~Array();
     Array(const Array& other);
     Array& operator=(const Array& other);
     Array operator&(const Array& other) const;
     bool contains(int value) const;
-    friend std::ostream& operator<<(std::ostream& os, const Array& arr);
-    friend std::istream& operator>>(std::istream& is, Array& arr);
+
+    friend std::ostream& operator<<(std::ostream& os, const Array& arr) {
+        os << "[";
+        for (int i = 0; i < arr.size; i++) {
+            os << arr.data[i];
+            if (i < arr.size - 1) {
+                os << ", ";
+            }
+        }
+        os << "]";
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, Array& arr) {
+        arr.freeMemory();
+        std::cout << "Enter the size of array: ";
+        int new_size = validInt();
+
+        if (new_size <= 0) {
+            throw std::invalid_argument("Size must be positive");
+        }
+
+        arr.allocateMemory(new_size);
+        std::cout << "Enter " << new_size << " elements:\n";
+        for (int i = 0; i < new_size; i++) {
+            std::cout << "Element " << i + 1 << ": ";
+            arr.data[i] = validInt();
+        }
+        return is;
+    }
 };
--- source/array.cpp
+++ source/array.cpp
@@ -18,9 +18,8 @@ void Array::freeMemory() {
     size = 0;
 }
 
-Array::Array() : data(nullptr), size(0) {}
-Array::Array(int array_size) : data(nullptr), size(0) {
-    if (size <= 0) {
+Array::Array(int array_size) : size(array_size) {
+    if (array_size <= 0) {
         throw std::invalid_argument("The size of array cannot be a negative number");
     }
     allocateMemory(size);
@@ -30,6 +29,7 @@ Array::~Array() { freeMemory(); }
 
 Array::Array(const Array& other) : data(nullptr), size(0) {
     if (other.data != nullptr && other.size > 0) {
+        size = other.size;
         allocateMemory(other.size);
         for (int i = 0; i < size; i++) {
             data[i] = other.data[i];
@@ -65,52 +65,25 @@ bool Array::contains(int value) const {
 
 Array Array::operator&(const Array& other) const {
     Array result;
-    if (data == nullptr || other.data == nullptr || size == 0 || other.size == 0) {
+    if (data == nullptr || size == 0) {
+        return result;
+    }
+    if (other.data == nullptr || other.size == 0) {
         return result;
     }
 
     for (int i = 0; i < size; i++) {
-        if (other.contains(data[i])) {
-            if (!result.contains(data[i])) {
-                int* new_data = new int[result.size + 1];
-                for (int j = 0; j < result.size; j++) {
-                    new_data[j] = result.data[j];
-                }
-                new_data[result.size] = data[i];
-                delete[] result.data;
-                result.data = new_data;
-                result.size++;
+        if (other.contains(data[i]) && !result.contains(data[i])) {
+            auto new_data = new int[result.size + 1];
+            for (int j = 0; j < result.size; j++) {
+                new_data[j] = result.data[j];
             }
+            new_data[result.size] = data[i];
+            delete[] result.data;
+            result.data = new_data;
+            result.size++;
         }
     }
     return result;
 }
 
-std::ostream& operator<<(std::ostream& os, const Array& arr) {
-    os << "[";
-    for (int i = 0; i < arr.size; i++) {
-        os << arr.data[i];
-        if (i < arr.size - 1) {
-            os << ", ";
-        }
-    }
-    os << "]";
-    return os;
-}
-
-std::istream& operator>>(std::istream& is, Array& arr) {
-    arr.freeMemory();
-    std::cout << "Enter the size of array: ";
-    int new_size = validInt();
-
-    if (new_size <= 0) {
-        throw std::invalid_argument("Size must be positive");
-    }
-    arr.allocateMemory(new_size);
-    std::cout << "Enter " << new_size << " elements:\n";
-    for (int i = 0; i < new_size; i++) {
-        std::cout << "Element " << i + 1 << ": ";
-        arr.data[i] = validInt();
-    }
-    return is;
-}
